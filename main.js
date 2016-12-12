d1c53f58ed9674acccdec9077f5857f6652b9177
Scott Humphries <git@sscotth.io>
2016-12-12 12:32:17 -0600

    eod

--- main.js
+++ main.js
@@ -4,3 +4,61 @@ console.log('Hello Friends')
 // document.querySelector('button.continue').innerHTML = "Next Step..."
 
 $('h1').html('Hello Friends')
+// document.querySelector('h1').innerHTML = "Hello Friends"
+// document.querySelectorAll('h1').innerHTML = "Hello Friends" // wont work because array-like NodeList
+
+// document ready function
+$(document).ready(function () {})
+
+// in vanilla javascript
+document.addEventListener('DOMContentLoaded', function () {})
+
+
+// append
+var foods = ['apple', 'banana', 'carrot', 'danish', 'egg']
+for (var i=0; i<foods.length; i++) {
+  $('ul').append(`<li>${foods[i]}</li>`)
+}
+
+// Goal: <div class="foods><ul>...</ul></div>
+
+// before and after
+// $('ul').before('<div class="foods">')
+// $('ul').after('</div>')
+
+
+// wrap
+$('ul').wrap('<div class="foods">')
+
+// assign a value
+$('input').first().val('Hello Friends')
+
+$('input').last().click(onClicky)
+
+function onClicky () {
+  console.log($('input').first().val())
+}
+
+$.getJSON('https://randomuser.me/api/?nat=us', function (data) {
+  var firstName = data.results[0].name.first
+  var lastName = data.results[0].name.last
+
+  $('h1').html(`Hello
+    ${firstName[0].toUpperCase()}${firstName.slice(1)}
+    ${lastName[0].toUpperCase()}${lastName.slice(1)}
+  `)
+})
+
+// or with a utility function
+
+// $.getJSON('https://randomuser.me/api/?nat=us', function (data) {
+//   $('h1').html(`Hello ${capitalize(firstName)} ${capitalize(lastName)}`)
+// })
+
+// function capitalize (str) {
+//   return `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`
+// }
+
+
+
+
