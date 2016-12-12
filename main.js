d0b34720f9bc3939d6958111a7ea2b67cbf35db5
Scott Humphries <git@sscotth.io>
2016-12-12 10:20:55 -0600

    first jquery example

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Hello Friends')
+
+// $( "button.continue" ).html( "Next Step..." ) is the same as:
+// document.querySelector('button.continue').innerHTML = "Next Step..."
+
+$('h1').html('Hello Friends')
